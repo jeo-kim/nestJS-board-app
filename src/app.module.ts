83d3ff6767b5dcf733db2e48e7ba189ea83a4e8e
jeonghyeon <skjh0807s@gmail.com>
2022-05-01 17:52:58 +0900

    create BoardsController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { BoardsModule } from './boards/boards.module';
 
 
 @Module({
-  imports: [],
+  imports: [BoardsModule],
 
 })
 export class AppModule {}
---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,5 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('boards')
+export class BoardsController {
+}

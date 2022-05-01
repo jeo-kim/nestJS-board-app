67f7a84f8d38c5b9242ea89b4abd346afce1cb93
jeonghyeon <skjh0807s@gmail.com>
2022-05-01 18:03:45 +0900

    create BoardsService

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
+    // Typescript는 javascript와 달리 private 같은 접근제어자를 사용할 수 있다.
+    // 접근제어자를 생성자 파라미터에 선언하면, 생성자 파라미터는 암묵적으로 클래스 프로퍼티로 사용된다.
+    constructor(private boardsService: BoardsService) {}
+
 }
